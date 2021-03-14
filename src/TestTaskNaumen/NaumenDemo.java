0bdb42a59dd2564c90a19debf922ffc53cbd8a14
kis <irsen.kan@bk.ru>
2021-03-14 15:16:28 +0300

    new1

--- src/TestTaskNaumen/NaumenDemo.java
+++ src/TestTaskNaumen/NaumenDemo.java
@@ -7,17 +7,17 @@ public class NaumenDemo {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         // приобразования в Int
-        int n = Integer.parseInt(in.nextLine());
-        String[] vectorN  = in.nextLine().split(" ");
+        int n = Integer.parseInt(in.nextLine()); //Принемает значения
+        String[] vectorN  = in.nextLine().split(" "); // Читает значения через пробел
 
         int m = Integer.parseInt(in.nextLine());
         String[] vectorM = in.nextLine().split(" ");
 
         int index = 0;
         for(String number: vectorN){
-            int cout = Integer.parseInt(number);
+            int count = Integer.parseInt(number); //
 
-            for(int i = 0; i < cout; i++){
+            for(int i = 0; i < count; i++){
                 System.out.println(vectorM[index]);
             }
             System.out.println(" ");
